efcca80cefb030725bfaa98dca54843da34e069a
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2024-01-08 11:28:44 +0200

    feat: Added credits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject credits;
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        credits.SetActive(false);
     }
 }
